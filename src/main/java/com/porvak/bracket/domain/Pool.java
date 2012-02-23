aae185983fa9042f31dfa47e80ab1a1898d89f1b
Omar Porras <omar@porvak.com>
2012-02-22 22:18:54 -0600

    ADDED: Scoring strategy to Pool, for calculating Leaderboard results.

--- src/main/java/com/porvak/bracket/domain/Pool.java
+++ src/main/java/com/porvak/bracket/domain/Pool.java
@@ -1,10 +1,21 @@
 package com.porvak.bracket.domain;
 
+import java.util.Map;
+
 public class Pool extends AbstractBracket{
     private String id;
     private String poolName;
     private String tournamentId;
     private PoolType poolType;
+    Map<Integer, Integer> scoringStrategy;
+
+    public Map<Integer, Integer> getScoringStrategy() {
+        return scoringStrategy;
+    }
+
+    public void setScoringStrategy(Map<Integer, Integer> scoringStrategy) {
+        this.scoringStrategy = scoringStrategy;
+    }
 
     public String getId() {
         return id;
