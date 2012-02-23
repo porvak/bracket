557a62ec3b1686fd5d30f449d1dd1762a2f27762
Omar Porras <omar@porvak.com>
2012-02-22 23:06:43 -0600

    CHANGED: updated tournament id.

--- src/main/webapp/resources/js/app/model/TournamentModel.js
+++ src/main/webapp/resources/js/app/model/TournamentModel.js
@@ -1,8 +1,10 @@
 (function() {
+
   define(['lib/backbone', 'base/jsonUri'], function(Backbone, jsonUri) {
     return Backbone.Model.extend({
       urlRoot: "" + jsonUri.root + "/tournament",
-      id: "1"
+      id: "4f41ce03d17060d0d8dbd4d6"
     });
   });
+
 }).call(this);
