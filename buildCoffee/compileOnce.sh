b9547dbd7dcf39f74ee04769d940419373636a41
Omar Porras <omar@porvak.com>
2012-02-24 01:03:14 -0600

    CHANGED: Updated script to be able to be run from any directory, if a variable called WORKSPACE is set. (For the build server)

--- buildCoffee/compileOnce.sh
+++ buildCoffee/compileOnce.sh
@@ -1,1 +1,7 @@
-coffee --lint  -o ../src/main/webapp/resources/js/ -c ../src/main/webapp/resources/coffee/
+#!/bin/bash
+
+if [ -n "$WORKSPACE" ]; then
+    coffee --lint  -o $WORKSPACE/src/main/webapp/resources/js/ -c $WORKSPACE/src/main/webapp/resources/coffee/
+else
+    coffee --lint  -o ../src/main/webapp/resources/js/ -c ../src/main/webapp/resources/coffee/
+fi
