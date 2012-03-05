fe168e209c9f99059399e999775b1c7dca8a267d
Omar Porras <omar@porvak.com>
2012-03-04 23:32:04 -0600

    CHANGED: refactored common conversion from Principal to Account to common base controller class.

--- src/main/java/com/porvak/bracket/controller/HomeController.java
+++ src/main/java/com/porvak/bracket/controller/HomeController.java
@@ -2,8 +2,6 @@ package com.porvak.bracket.controller;
 
 import com.porvak.bracket.domain.Tournament;
 import com.porvak.bracket.repository.TournamentRepository;
-import com.porvak.bracket.socialize.account.Account;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,13 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import java.security.Principal;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @Controller
-public class HomeController {
+public class HomeController extends AbstractBracketController {
 
     @Inject
     private TournamentRepository tournamentRepository;
@@ -26,19 +23,16 @@ public class HomeController {
     @Inject
     private ConnectionRepository connectionRepository;
 
-    @Inject @Named("bracketConversionService")
-    private ConversionService conversionService;
-    
     @RequestMapping("/")
     public String home(Principal currentUser, Model model) {
         if (currentUser != null) {
             model.addAttribute("twitter_status", connectionRepository.findConnections("twitter").size() > 0 ? "Yes" : "No");
-            model.addAttribute(conversionService.convert(currentUser, Account.class));
+            model.addAttribute(getUserAccount(currentUser));
         }
 
         return "index";
     }
-    
+
     @ResponseBody
     @RequestMapping(value = "/api/pub/tournament/{id}", method = GET)
     public Tournament getTournamentById(@PathVariable("id") String id){
