a1eed06ea5bf186e9b10c89ee3b7cae05077c60e
Omar Porras <omar@porvak.com>
2012-03-17 12:35:41 -0500

    Ignoring integration test.

--- src/test/java/com/porvak/bracket/repository/impl/BracketMapReduceImplTest.java
+++ src/test/java/com/porvak/bracket/repository/impl/BracketMapReduceImplTest.java
@@ -3,6 +3,7 @@ package com.porvak.bracket.repository.impl;
 import com.porvak.bracket.config.ComponentConfig;
 import com.porvak.bracket.config.EmbeddedDataConfig;
 import com.porvak.bracket.config.TestSecurityConfig;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
@@ -19,7 +20,7 @@ public class BracketMapReduceImplTest {
     @Inject
     BracketMapReduceImpl bracketMapReduce;
 
-    @Test
+    @Test @Ignore
     public void testGenerateLeaderboard() throws Exception {
         bracketMapReduce.generateLeaderboard();
     }
