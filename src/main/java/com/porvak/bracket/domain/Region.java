9146090e9a1a36ea83bd569982aa1126ee21806d
Omar Porras <omar@porvak.com>
2012-03-17 12:22:58 -0500

    REMOVED: commented out code.

--- src/main/java/com/porvak/bracket/domain/Region.java
+++ src/main/java/com/porvak/bracket/domain/Region.java
@@ -19,10 +19,6 @@ public class Region extends AbstractBracket{
 
     private String name;
 
-//    @JsonProperty
-//    @Transient
-//    private List<Round> rounds;
-
     @JsonIgnore
     @Field("rounds")
     private Map<Integer, Round> roundMap;
